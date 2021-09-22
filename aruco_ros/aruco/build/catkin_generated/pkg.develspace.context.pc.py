cfae930461d17879dc395d96af469ccef0817a05
Simone Contento <80551374+s-contento@users.noreply.github.com>
2021-09-22 17:55:43 +0200

    Delete aruco_ros directory

--- aruco_ros/aruco/build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/jcacace/ros_ws/src/aruco_ros/aruco/include".split(';') if "/home/jcacace/ros_ws/src/aruco_ros/aruco/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-laruco".split(';') if "-laruco" != "" else []
-PROJECT_NAME = "aruco"
-PROJECT_SPACE_DIR = "/home/jcacace/ros_ws/src/aruco_ros/aruco/build/devel"
-PROJECT_VERSION = "2.1.1"
